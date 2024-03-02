d9f516c98b25847d321174c53935feec801f984d
James Winnett <jam.winnett@gmail.com>
2024-03-02 12:28:20 +0000

    Can detect key presses in window

--- OpenGL_Engine/Window_GL.cpp
+++ OpenGL_Engine/Window_GL.cpp
@@ -1,100 +1,127 @@
 #include "Window_GL.h"
 #include <iostream>
 
+void Window_GL::framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	Window_GL* instance = static_cast<Window_GL*>(glfwGetWindowUserPointer(window));
+	if (instance == nullptr) { return; }
+
+	if (action == GLFW_PRESS) {
+		instance->SetLastKey(key);
+		std::cout << "Key pressed: " << key << std::endl;
+	}
+
+	//    // lastPressedKey = key;
+	//    SetLastKey(key);
+	//    std::cout << "Key pressed: " << key << std::endl;
+}
 
 Window_GL::Window_GL(Game* game, const int width, const int height)
 {
 	_width = width;
 	_height = height;
-    _game = game;
+	_game = game;
+	lastPressedKey = -1;
 }
 
 int Window_GL::Initialise(const char* pTitle)
 {
-    SetTitle(pTitle);
-    _renderer = new Renderer_GL();
+	SetTitle(pTitle);
+	_renderer = new Renderer_GL();
 
-    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
-    // glfw: initialize and configure
+	// glfw: initialize and configure
    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// glfw window creation
+	// --------------------
+	_window = glfwCreateWindow(_width, _height, _title.c_str(), NULL, NULL);
+	if (_window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods);
+	glfwSetKeyCallback(_window, key_callback);
+
+	glfwSetWindowUserPointer(_window, this);
 
 
-    // glfw window creation
-    // --------------------
-    _window = glfwCreateWindow(_width, _height, _title.c_str(), NULL, NULL);
-    if (_window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(_window);
-    glfwSetFramebufferSizeCallback(_window, framebuffer_size_callback);
+	glfwMakeContextCurrent(_window);
+	// TheWindow = static_cast<Window_GL*>(glfwGetWindowUserPointer(_window));
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
 
-    //while (!glfwWindowShouldClose(window))
-    //{
-    //    // input
-    //    // -----
-    //    processInput(window);
+	glfwSetFramebufferSizeCallback(_window, framebuffer_size_callback);
 
-    //    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-    //    // -------------------------------------------------------------------------------
-    //    glfwSwapBuffers(window);
-    //    glfwPollEvents();
-    //}
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    // glfwTerminate();
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//while (!glfwWindowShouldClose(window))
+	//{
+	//    // input
+	//    // -----
+	//    processInput(window);
+
+	//    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+	//    // -------------------------------------------------------------------------------
+	//    glfwSwapBuffers(window);
+	//    glfwPollEvents();
+	//}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	// glfwTerminate();
 }
 
-void Window_GL::processInput(GLFWwindow* window)
+void Window_GL::OnKeyboard(GLFWwindow* window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(window, true);
-        Close();
-    }
+
+	if (glfwGetKey(window, 256) == 1)
+	{
+		glfwSetWindowShouldClose(window, true);
+		Close();
+	}
 }
 
 
-void Window_GL::framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
+
+
 
 void Window_GL::Close()
 {
-    glfwTerminate();
+	glfwTerminate();
 }
 
 void Window_GL::Update()
 {
-    processInput(_window);
+	// OnKeyboard(_window);
 
-     glfwSwapBuffers(_window);
-     glfwPollEvents();
+	glfwSwapBuffers(_window);
+	glfwPollEvents();
 }
 
 void Window_GL::SetTitle(const char* pTitle)
 {
-    _title = pTitle;
+	_title = pTitle;
 }
 
 Window_GL::~Window_GL()
 {
-    delete _renderer;
+	delete _renderer;
 }
--- OpenGL_Engine/Window_GL.h
+++ OpenGL_Engine/Window_GL.h
@@ -9,16 +9,20 @@
 class Window_GL : public Window
 {
 	GLFWwindow* _window;
+	int lastPressedKey;
 public:
+	void SetLastKey(int pKey ) { lastPressedKey = pKey; }
 
 	Window_GL(Game* game, const int width, const int height);
 	virtual int Initialise(const char* pTitle);
 
 	//virtual void OnResize( int width, int height) override;
 
-	void processInput(GLFWwindow* window);
+	void OnKeyboard(GLFWwindow* window);
 
 	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+
 	virtual void Close();
 
 	virtual void Update();
@@ -26,4 +30,5 @@ public:
 
 	virtual ~Window_GL();
 };
+	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 #endif // OPENGL
