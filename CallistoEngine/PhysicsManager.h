410e7a94a423051306a0118a8b707b90b31d99e9
JammyJ1mJ1m <jam.winnett@gmail.com>
2024-06-02 22:31:56 +0100

    Fleshed out physics manager

--- CallistoEngine/PhysicsManager.h
+++ CallistoEngine/PhysicsManager.h
@@ -1,5 +1,30 @@
 #pragma once
+#include "Bullet/btBulletDynamicsCommon.h"
+
 class PhysicsManager
 {
-};
+	btDiscreteDynamicsWorld* mDynamicsWorld;
+	btDefaultCollisionConfiguration* mCollisionConfiguration;
+	btCollisionDispatcher* mDispatcher;
+	btDbvtBroadphase* mBroadphase;
+	btSequentialImpulseConstraintSolver* mSolver;
 
+	float timeAccumulator = 0.0f;
+	float mGravity;
+
+public:
+	static const float mTimeStep;
+
+	PhysicsManager(float pGrav = -9.8f);
+	~PhysicsManager();
+
+	void SetGravity(float gravity) { mGravity = gravity; }
+	float GetGravity() { return mGravity; }
+
+	inline btDiscreteDynamicsWorld& GetDynamicsWorld() { return *mDynamicsWorld; }
+
+	void Initialise();
+
+	// steps through the physics 
+	void Update(double deltaTime);
+};
