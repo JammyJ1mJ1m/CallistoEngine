4e6b95c5d9aa0ddb6f53a49088dde09b5f3c8450
JammyJ1mJ1m <jam.winnett@gmail.com>
2024-06-03 16:32:14 +0100

    Two library folders

--- ShooterGame/Enemy.cpp
+++ ShooterGame/Enemy.cpp
@@ -1,4 +1,5 @@
 #include "Enemy.h"
+#include "Components/ComponentRigidBody.h"
 
 void Enemy::start()
 {
@@ -6,6 +7,18 @@ void Enemy::start()
 	AddComponent(new ComponentShaderDefault(ShooterGame::GetGameCamera(), "Resources/Shaders/default.vert", "Resources/Shaders/default.frag"));
 	AddComponent(new ComponentTransform(glm::vec3(2.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f)));
 	//mEntities.push_back(tri);
+
+	btVector3 halfExtents(1.0, 1.0, 1.0); // This will create a box of size 2x2x2
+	btCollisionShape* boxShape = new btBoxShape(halfExtents);
+
+	// empty btTransform
+
+	btTransform transform;
+	transform.setIdentity();
+	transform.setOrigin(btVector3(2.0f, 0.0f, 0.0f)); // Starting at position (2.0f, 0.0f, 0.0f)
+
+	ComponentRigidBody* rb = new ComponentRigidBody(boxShape, 1.0f, btVector3(0, 0, 0), transform);
+	AddComponent(rb);
 }
 
 Enemy::Enemy()
