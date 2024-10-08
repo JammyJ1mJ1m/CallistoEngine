b5edf58d199afa4774aadd74b35940691a6cdb14
JammyJ1mJ1m <jam.winnett@gmail.com>
2024-10-08 16:58:52 +0100

    Added spawn pop sound

--- ShooterGame/Entities/Enemy.cpp
+++ ShooterGame/Entities/Enemy.cpp
@@ -4,6 +4,8 @@
 //#include "Bullet/BulletCollision/Gimpact/btGImpactCollisionAlgorithm.h"	
 #include "Components/ComponentCollider.h"
 #include "Math/Vector.h"
+#include "Misc/Sound.h"
+
 
 
 
@@ -14,27 +16,18 @@ void Enemy::start()
 	Vector3f pos(0.0f, 5.0f, 0.0f);
 	AddComponent(new ComponentTransform(pos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f)));
 
-	//Vector3f newPos;
-	//newPos.SetX(pos.x);
-	//newPos.SetY(pos.y);
-	//newPos.SetZ(pos.z);
+
 	ComponentCollider* collider = new ComponentCollider(Game::GetGame()->GetMesh("cube")->GetVertices(),GetComponent<ComponentTransform>()->GetScale().s	 );
-
-	//pos[0] = collider->GetCenter().GetX();
-	//pos[1] = collider->GetCenter().GetY();
-	//pos[2] = collider->GetCenter().GetZ();
-
-
-	//ComponentCollider* collider = new ComponentCollider(btVector3(0.177980006, 0.989475012, 4.58138275));
-	//ComponentCollider* collider = new ComponentCollider(1);
-
 	AddComponent(collider);
 
 	btScalar mass = 1.0;
-	//glm::vec3 posi = glm::vec3(GetComponent<ComponentCollider>()->GetCenter().GetX(), GetComponent<ComponentCollider>()->GetCenter().GetY(), GetComponent<ComponentCollider>()->GetCenter().GetZ());
-
 	ComponentRigidBody* rb = new ComponentRigidBody(collider, mass, pos);
 	AddComponent(rb);
+
+	Sound* sound = new Sound("Resources/Sounds/Pop.wav");
+
+	 sound->Play3D(pos.GetX(),pos.GetY(),pos.GetZ());
+
 }
 
 void Enemy::SetPosition(const Vector3f& pPosition)
